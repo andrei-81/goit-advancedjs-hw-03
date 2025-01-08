e6d50451c47b675c4b8879b8830ea4922f2afcd2
AndriHordiichuk <166404529+AndriHordiichuk@users.noreply.github.com>
2025-01-08 02:21:05 +0200

    img

--- src/js/render-function.js
+++ src/js/render-function.js
@@ -15,7 +15,7 @@ const iziToastParameters = {
     messageSize: "16px",
     messageColor: "#FAFAFB",
     backgroundColor: "#EF4040",
-    iconUrl: '../img/octagon.png',
+    iconUrl: "../img/octagon.png",
     iconColor: "#FAFAFB",
     maxWidth: 432,  
     messageLineHeight: "24px",
